ca9f0b67c7170e24edd148debf837314f68a17a6
Steven Pescheteau <steven.pescheteau@gmail.com>
2020-02-15 09:04:12 +0100

    partial templates OK

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,8 +2,6 @@ const EXPRESS = require('express');
 const ROUTER = EXPRESS.Router();
 
 ROUTER.get('/', function (req, res) {
-    console.log(req);
-    console.log(req.session);
     res.render('dashboard', { guilds : req.session.guilds });
 });
 
