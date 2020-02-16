c19844413ac83490c9100559005a2033dba032f9
Steven Pescheteau <steven.pescheteau@gmail.com>
2020-02-16 20:58:17 +0100

    guilds ejs/route removed

--- routes/guilds.js
+++
@@ -1,7 +0,0 @@
-const EXPRESS = require('express');
-const ROUTER = EXPRESS.Router();
-
-ROUTER.get('/', (req, res) => res.render('guilds', 
-    {user : req.session.user, guilds : req.session.guilds }));
-
-module.exports = ROUTER;
