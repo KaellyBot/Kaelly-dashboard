80804c44c797ae24e503fc762699199f80845f1a
Steven Pescheteau <steven.pescheteau@gmail.com>
2020-02-17 23:23:33 +0100

    dashboard by guild first template ok

--- public/js/init.js
+++ public/js/init.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     var sideNav = M.Sidenav.init(document.querySelectorAll('.sidenav'), {});
     var dropdowns = M.Dropdown.init(document.querySelectorAll('.dropdown-trigger'), {});
+    var selects = M.FormSelect.init(document.querySelectorAll('select'), {});
+    var tabs = M.Tabs.init(document.querySelectorAll('.tabs'), {
+        onShow: tab => M.Tabs.init(document.querySelectorAll('.nested'), {})
+        });
 });
